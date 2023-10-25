e25c00f696dd3f837a492837e00a8043c09fda4f
GREEN <GREEN@G704-04>
2023-10-25 18:02:11 +0900

    practice

--- 10_class/classes/Exam24_Address.java
+++ 10_class/classes/Exam24_Address.java
@@ -2,4 +2,49 @@ package classes;
 
 public class Exam24_Address {
 	// pdf 360
+	private String name;
+	private String tel;
+	private String addr;
+
+	// getter
+	public String getName() {
+		return name;
+	}
+
+	public String getTel() {
+		return tel;
+	}
+
+	public String getAddr() {
+		return addr;
+	}
+
+	// setter
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+
+	public Exam24_Address() {
+
+	}
+
+	public Exam24_Address(String name, String tel, String addr) {
+		super();
+		this.name = name;
+		this.tel = tel;
+		this.addr = addr;
+	}
+
+	public String showAddress() {
+		return "이름 : " + name + "\n전화 : " + tel + "\n주소 : " + addr;
+	}
+
 }
--- 10_class/classes/Exam24_Review.java
+++ 10_class/classes/Exam24_Review.java
@@ -2,4 +2,10 @@ package classes;
 
 public class Exam24_Review {
 	// pdf 360 실행클래스
+
+	public static void main(String[] args) {
+		Exam24_Address mem01 = new Exam24_Address("박서진", "010-1234-5678", "서울시 강남구 역삼동");
+		System.out.println(mem01.showAddress());
+	}
+
 }
